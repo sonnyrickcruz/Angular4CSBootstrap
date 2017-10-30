6ebc978c5c2bc9a29a34b3b15f4a3f6d04edc98a
sonnyrickcruz <sonny.cruz@pointwest.com.ph>
2017-10-30 16:39:59 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { SkillCatalogComponent } from './components/skill-catalog/skill-catalog.component';
+import { ExploreComponent } from './components/explore/explore.component';
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'skill-catalog', component: SkillCatalogComponent},
+  {path: 'explore', component: ExploreComponent},
+  // {path: 'navbar', component: NavbarComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomeComponent,
+    SkillCatalogComponent,
+    ExploreComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
